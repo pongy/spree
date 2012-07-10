940bee084ec8c6017988cba753f44ca1fd4e8fe6
Ken Yuen <kenpong74@gmail.com>
2012-07-10 17:32:37 +0800

    revert to order version

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -62,8 +62,7 @@ module Spree
       Image.joins("LEFT JOIN #{Variant.quoted_table_name} ON #{Variant.quoted_table_name}.id = #{Asset.quoted_table_name}.viewable_id").
       where("#{Variant.quoted_table_name}.product_id = #{self.id}").
       order("#{Asset.quoted_table_name}.position").
-      extend(Spree::Core::RelationSerialization).
-      all
+      extend(Spree::Core::RelationSerialization)
     end
 
     alias_method :images, :variant_images
